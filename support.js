d6bd908b1b501b75b8a00751f957bfe3d12e5fcb
Amanpreet Dodd <amanpreetdodd@Dev-AmanDodd.local>
2017-05-26 15:09:21 -0400

    new support file

---
+++ support.js
@@ -0,0 +1,16 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+module.exports = [
+    // destination
+    function(session) {
+        // richards function values will be placed here
+        // then call send
+        //session.send(....);    
+    },
+
+
+
+
+
+];
