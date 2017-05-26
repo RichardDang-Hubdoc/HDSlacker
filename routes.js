5a894edd4ef5b76d7c0e1d0ab7d2dac78d3c82a5
Richard Dang <richarddang@hubdoc.com>
2017-05-26 15:31:12 -0400

    Fixed query callback issue

--- routes.js
+++ routes.js
@@ -2,9 +2,8 @@ const pool = require('./db.js');
 
 module.exports = function(server) {
 
-    var support_stats = [];
-
     server.get('/support_stats', (req, res, next) => {
+        var support_stats = [];
 
         pool.query(`SELECT
                     date_trunc('week', ticket_created_at) as date,
@@ -20,60 +19,53 @@ module.exports = function(server) {
             if (err) {
                 return console.error('error running query', err);
             }
-
             results.rows.forEach(function(row, i) {
                 support_stats.push({
                     date: row.date,
                     first_resp_1_hour: (row.replies_1_hour / row.new_tickets * 100).toFixed(2),
                     avg_first_reply_time: row.avg_reply_time,
                     new_tickets: row.new_tickets,
+                    solved_tickets: null,
+                    backlog: null
+                });
+            });
+            pool.query(`SELECT
+                                date_trunc('week', solved_at) as date,
+                                count(*) as solved_tickets
+                                FROM zendesk_tickets
+                                WHERE status IN ('closed', 'status')
+                                AND solved_at IS NOT NULL
+                                AND has_metrics
+                                GROUP BY date
+                                ORDER BY date DESC
+                                LIMIT 2;
+            `, function(err, results) {
+                if (err) {
+                    return console.error('error running query', err);
+                }
+                results.rows.forEach(function(row, i) {
+                    support_stats[i].solved_tickets = row.solved_tickets;
+                });
+                pool.query(`SELECT dd.d AS date,
+                                    count(*) AS backlog
+                                    FROM generate_series(date_trunc('week',now() - interval '2 months'), date_trunc('week', now()), '1 week'::interval) dd(d)
+                                    INNER JOIN zendesk_tickets z ON (date_trunc('week',z.ticket_created_at) <= dd.d
+                                    AND (z.solved_at IS NULL OR date_trunc('week',z.solved_at) > dd.d))
+                                    WHERE (z.has_metrics)
+                                    AND (status NOT IN ('deleted', 'solved', 'closed'))
+                                    GROUP BY date
+                                    ORDER BY date DESC
+                                    LIMIT 2;
+                `, function(err, results) {
+                    if (err) {
+                        return console.error('error running query', err);
+                    }
+                    results.rows.forEach(function(row, i) {
+                        support_stats[i].backlog = row.backlog;
+                    });
+                    res.send(support_stats);
                 });
             });
         });
-
-
-
-        pool.query(`SELECT
-                    date_trunc('week', solved_at) as date,
-                    count(*) as solved_tickets
-                    FROM zendesk_tickets
-                    WHERE status IN ('closed', 'status')
-                    AND solved_at IS NOT NULL
-                    AND has_metrics
-                    GROUP BY date
-                    ORDER BY date DESC
-                    LIMIT 2;
-`, function(err, results) {
-            if (err) {
-                return console.error('error running query', err);
-            }
-
-            results.rows.forEach(function(row, i) {
-                support_stats[i].solved_tickets = row.solved_tickets;
-            });
-        });
-
-        pool.query(`SELECT dd.d AS date,
-                    count(*) AS backlog
-                    FROM generate_series(date_trunc('week',now() - interval '2 months'), date_trunc('week', now()), '1 week'::interval) dd(d)
-                    INNER JOIN zendesk_tickets z ON (date_trunc('week',z.ticket_created_at) <= dd.d
-                    AND (z.solved_at IS NULL OR date_trunc('week',z.solved_at) > dd.d))
-                    WHERE (z.has_metrics)
-                    AND (status NOT IN ('deleted', 'solved', 'closed'))
-                    GROUP BY date
-                    ORDER BY date DESC
-                    LIMIT 2;
-`, function(err, results) {
-            if (err) {
-                return console.error('error running query', err);
-            }
-
-            results.rows.forEach(function(row, i) {
-                support_stats[i].backlog = row.backlog;
-            });
-        });
-
-        res.send(support_stats);
-
     });
 };
